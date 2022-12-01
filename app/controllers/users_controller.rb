fb0c1d5e8d2380833011f6e7d8227917f9168960
yym-yumeng123 <18026493873@163.com>
2022-12-01 16:13:46 +0800

    controller before_action 的使用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :auth_user, only: [:index]
+  def index
+    @users = User.all
+  end
+  
+
   def new
     @user = User.new
   end
@@ -11,6 +17,14 @@ class UsersController < ApplicationController
       redirect_to new_session_path
     else
       render action: :new
+    end
+  end
+
+  private
+  def auth_user
+    unless session[:user_id]
+      flash[:notice] = '请登录'
+      redirect_to new_session_path
     end
   end
 end
