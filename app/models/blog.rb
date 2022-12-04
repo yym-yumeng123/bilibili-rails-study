311fefa9c4b95a4caffdde689eddd88f77ae8e77
yym-yumeng123 <18026493873@163.com>
2022-12-04 22:27:48 +0800

    修复 bug

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   # has_and_belongs_to_many :tags, join_table: "table_name", foreign_key: "tags_id"
 
   # class_name 模型名
-  has_many :blogs_tags, class_name: 'BlogTag'
+  has_many :blogs_tags, class_name: 'BlogsTags'
   has_many :tags, through: :blogs_tags
 
   validates :title, presence: {message: '标题不能为空'}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   # has_and_belongs_to_many :blogs
-  has_many :blogs_tags, class_name: 'BlogTag'
+  has_many :blogs_tags, class_name: 'BlogsTags'
   has_many :blogs, through: :blogs_tags
 
   validates_presence_of :title, message: '标签不能为空'
